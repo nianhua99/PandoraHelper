a316403ca53234cba1ee2e641986e7e17da9161a
nianhua99 <1139168548@qq.com>
2024-09-19 22:40:27 +0800

    feat: 支持对接oneapi/newapi

--- frontend/dist/assets/index-cb661052.js
+++
@@ -1,1 +0,0 @@
-import{w as e,H as s,F as t,x as a,y as o,z as n}from"./index-24044c4a.js";const r=e;r.Header=s,r.Footer=t,r.Content=a,r.Sider=o,r._InternalSiderContext=n;const d=r;export{d as L};
---
+++ frontend/dist/assets/index-e8d0f625.js
@@ -0,0 +1,1 @@
+import{w as e,H as s,F as t,x as a,y as o,z as n}from"./index-7b122661.js";const r=e;r.Header=s,r.Footer=t,r.Content=a,r.Sider=o,r._InternalSiderContext=n;const d=r;export{d as L};
